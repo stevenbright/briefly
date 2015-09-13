1bc2d713c48dde288aefa640feea620e0e21729e
Steven <vstrength@gmail.com>
2015-09-12 18:57:18 -0700

    Setting project skeleton

---
+++ briefly-website/src/main/java/com/alexshabanov/sample/brikar/website/Launcher.java
@@ -0,0 +1,18 @@
+package com.alexshabanov.sample.brikar.website;
+
+import com.truward.brikar.server.launcher.StandardLauncher;
+
+/**
+ * Entry point
+ */
+public final class Launcher {
+  public static void main(String[] args) throws Exception {
+    try (StandardLauncher launcher = new StandardLauncher("classpath:/brieflyWebsite/")) {
+      launcher
+          .setSpringSecurityEnabled(true)
+          .setSessionsEnabled(true)
+          .setStaticHandlerEnabled(true)
+          .start();
+    }
+  }
+}
---
+++ briefly-website/src/main/java/com/alexshabanov/sample/brikar/website/controller/PublicPageController.java
@@ -0,0 +1,48 @@
+package com.alexshabanov.sample.brikar.website.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Main controller that responds with user page.
+ */
+@Controller
+@RequestMapping("/g/")
+public final class PublicPageController {
+//  private ArticleService articleService;
+//  private VisitorService visitorService;
+//
+//  public PublicPageController(ArticleService articleService, VisitorService visitorService) {
+//    this.articleService = Objects.requireNonNull(articleService, "articleService");
+//    this.visitorService = Objects.requireNonNull(visitorService, "visitorService");
+//  }
+
+  @RequestMapping("/index")
+  public String index() {
+    return "page/index";
+  }
+
+  @RequestMapping("/articles")
+  public ModelAndView articles() {
+    return new ModelAndView("page/articles", "articles", Collections.emptyList());
+  }
+
+  @RequestMapping("/about")
+  public String about() {
+    return "page/about";
+  }
+
+  @RequestMapping("/login")
+  public ModelAndView login(@RequestParam(value = "error", required = false) String loginError) {
+    final Map<String, Object> params = new HashMap<>();
+    params.put("loginError", loginError);
+    params.put("currentTime", System.currentTimeMillis());
+    return new ModelAndView("page/login", params);
+  }
+}
