cecc290103913e562dae0dfa20a505e46ed574d6
Steven <vstrength@gmail.com>
2015-12-22 14:21:26 +0300

    Adding catalog page loading

--- briefly-static-content/js/view/catalog/CatalogPage.react.js
+++ briefly-static-content/js/view/catalog/CatalogPage.react.js
@@ -1,15 +1,43 @@
 'use strict';
 
 import React, {Component} from 'react';
+import LoadingPage from '../common/LoadingPage.react';
 
-export default class CatalogPage extends Component<{}, {}, {}> {
+import CatalogAdapterService from '../../service/CatalogAdapterService';
+
+export default class CatalogPage extends Component<{},
+  /* Props */{ },
+  /* State */{ loading: boolean, items: array, offsetToken: ?string }> {
+
+  state = {
+    loading: true,
+    items: [],
+    offsetToken: null
+  }
+
+  componentDidMount(): void {
+    this._fetch("book", null, 10);
+  }
+
   render(): ?ReactElement {
+    if (this.state.loading) {
+      return <LoadingPage target="Catalog" />;
+    }
+
+    const itemStr = JSON.stringify(this.state.items);
+
     return (
       <div className="container">
         <h2>Catalog Page</h2>
         <hr/>
-        <p>TODO: content</p>
+        <p>offsetToken: {this.state.offsetToken}</p>
+        <p>itemStr: {itemStr}</p>
       </div>
     );
+  }
+
+  _fetch(itemType: string, offsetToken: ?string, limit: number): void {
+    const p = CatalogAdapterService.getItemByType(itemType, offsetToken, limit);
+    p.then((response) => this.setState({items: response.items, offsetToken: response.offsetToken, loading: false}));
   }
 }
