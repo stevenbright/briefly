ab7f67fe0949f772de0c807b612ed660285de84c
Steven <vstrength@gmail.com>
2016-03-06 10:50:43 -0800

    Adding briefly-db-init

---
+++ scripts/briefly-db-init/src/main/java/briefly/dbinit/service/MigrationService.java
@@ -0,0 +1,40 @@
+package briefly.dbinit.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class MigrationService {
+  public interface Contract {
+    void runMigration();
+  }
+
+  public static final class Impl implements Contract {
+    private final Logger log = LoggerFactory.getLogger(getClass());
+    private final DataTransferService.Contract dataTransferService;
+
+    public Impl(DataTransferService.Contract dataTransferService) {
+      this.dataTransferService = dataTransferService;
+    }
+
+    @Override
+    public void runMigration() {
+      log.info("About to run migration...");
+
+      if (!dataTransferService.prepare()) {
+        return;
+      }
+
+      for (Long startId = null;;) {
+        log.info("Moving next chunk of data");
+        startId = dataTransferService.transferNext(startId);
+        if (startId == null) {
+          break;
+        }
+      }
+
+      dataTransferService.complete();
+
+      log.info("Migration Completed.");
+    }
+  }
+}
---
+++ scripts/briefly-db-init/src/main/lombok/briefly/dbinit/model/BookMeta.java
@@ -0,0 +1,25 @@
+package briefly.dbinit.model;
+
+import com.truward.time.UtcTime;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.util.Date;
+
+/**
+ * Represents book meta information
+ */
+@EqualsAndHashCode
+@ToString
+@Getter
+@AllArgsConstructor
+public class BookMeta {
+  private Long id;
+  private String title;
+  private int fileSize;
+  private UtcTime dateAdded;
+  private Long langId;
+  private Long originId;
+}
